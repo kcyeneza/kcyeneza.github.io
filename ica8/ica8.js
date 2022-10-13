db8335b9c00a039d8916f23cb6b61d84f8cdbed4
kcyeneza <70969620+kcyeneza@users.noreply.github.com>
2022-10-13 00:10:37 -0600

    changes
    changes

---
+++ ica8/ica8.js
@@ -0,0 +1,10 @@
+let body = document.querySelector("body");
+body.addEventListener('click', giveAlert());
+function giveAlert (){
+    alert("I'M THE BADDEST ALIVE!");
+}
+
+body.addEventListener('keypress', function (){coolFunction(h1);});
+{
+    alert("DO YOU WANNA DANCE BABY!");
+}
