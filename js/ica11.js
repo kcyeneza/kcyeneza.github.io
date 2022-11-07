351e47215f81a86bc1a6d721774664122063c525
kcyeneza <70969620+kcyeneza@users.noreply.github.com>
2022-11-06 17:18:22 -0700

    changes
    changes

--- js/ica11.js
+++ js/ica11.js
@@ -1,7 +1,7 @@
 const quoteButton = document.querySelector('.new-quote');
 quoteButton.addEventListener('click', getQuote);
 
-const endpoint = 'https://www.boredapi.com/api/activity';
+const endpoint = 'https://v2.jokeapi.dev/joke/Any?safe-mode';
 
 async function getQuote() {
     console.log('test click worked');
@@ -11,11 +11,11 @@ async function getQuote() {
 
     let json_response = JSON.parse(response);
     // console.log(json_response);
-    console.log(json_response["activity"]);
+    console.log(json_response["joke"]);
 
     // .then(text => text.text());
     // console.log(text('message'));
-    displayQuote(json_response['activity']);
+    displayQuote(json_response['joke']);
 }
 
 function displayQuote(x) {
