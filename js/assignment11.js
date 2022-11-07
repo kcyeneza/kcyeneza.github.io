9c6330e19eb0e90279d0f0cf9b18f242e66fd55d
kcyeneza <70969620+kcyeneza@users.noreply.github.com>
2022-11-06 20:03:52 -0700

    changes
    changesc

--- js/assignment11.js
+++ js/assignment11.js
@@ -7,12 +7,6 @@ emojiButton.addEventListener('click', emojiDoer);
 
 const endpoint = 'https://v2.jokeapi.dev/joke/Any?safe-mode';
 
-/*function emojiDoer(){
-
-        let pic = <img src="https://i.guim.co.uk/img/media/a1b7129c950433c9919f5670c92ef83aa1c682d9/55_344_1971_1183/master/1971.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=88ba2531f114b9b58b9cb2d8e723abe1" alt="HAHAHA"/>
-                
-    }
-*/
 async function getQuote() {
     console.log('test click worked');
     let text = await fetch(endpoint);
