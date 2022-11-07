66814b9e2b5063aa73bc52ae496109f354c0cfe2
kcyeneza <70969620+kcyeneza@users.noreply.github.com>
2022-11-06 19:56:22 -0700

    changes
    changes

--- js/assignment11.js
+++ js/assignment11.js
@@ -1,7 +1,17 @@
 const quoteButton = document.querySelector('.new-quote');
 quoteButton.addEventListener('click', getQuote);
+const emojiButton= document.querySelector('.emoji');
+emojiButton.addEventListener('click', emojiDoer);
+
+
 
 const endpoint = 'https://v2.jokeapi.dev/joke/Any?safe-mode';
+
+function emojiDoer(){
+
+        let pic = <img src="https://i.guim.co.uk/img/media/a1b7129c950433c9919f5670c92ef83aa1c682d9/55_344_1971_1183/master/1971.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=88ba2531f114b9b58b9cb2d8e723abe1" alt="HAHAHA"/>
+                
+    }
 
 async function getQuote() {
     console.log('test click worked');
@@ -26,6 +36,8 @@ function displayQuote(x) {
     //quoteBox.appendChild(textMessage);
     document.getElementById('js-quote-text').textContent = x;
 }
+
+
 
 //gets Quote on screen
 getQuote();
