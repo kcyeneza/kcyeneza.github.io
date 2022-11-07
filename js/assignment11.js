9e955f2dcd6fdcd6fa1dd9732a137da9ecca24e2
kcyeneza <70969620+kcyeneza@users.noreply.github.com>
2022-11-06 18:04:16 -0700

    changes
    changes

---
+++ js/assignment11.js
@@ -0,0 +1,34 @@
+const quoteButton = document.querySelector('.new-quote');
+quoteButton.addEventListener('click', getQuote);
+
+const endpoint = 'https://v2.jokeapi.dev/joke/Any?safe-mode';
+
+async function getQuote() {
+    console.log('test click worked');
+    let text = await fetch(endpoint);
+    let response = await text.text();
+
+
+    let json_response = JSON.parse(response);
+    // console.log(json_response);
+    console.log(json_response["joke"]);
+
+    // .then(text => text.text());
+    // console.log(text('message'));
+    displayQuote(json_response['joke']);
+}
+
+function displayQuote(x) {
+    const quoteBox = document.querySelector('#js-quote-text');
+    // console.log('display quote working');
+    //const textMessage = document.createTextNode(x);
+
+    //quoteBox.appendChild(textMessage);
+    document.getElementById('js-quote-text').textContent = x;
+}
+
+//gets Quote on screen
+getQuote();
+
+
+
--- js/ica11.js
+++ js/ica11.js
@@ -1,34 +1,31 @@
 const quoteButton = document.querySelector('.new-quote');
 quoteButton.addEventListener('click', getQuote);
 
-const endpoint = 'https://v2.jokeapi.dev/joke/Any?safe-mode';
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 
 async function getQuote() {
-    console.log('test click worked');
-    let text = await fetch(endpoint);
-    let response = await text.text();
+    console.log('test click worked');
+    let text = await fetch(endpoint);
+    let response = await text.text();
 
 
-    let json_response = JSON.parse(response);
-    // console.log(json_response);
-    console.log(json_response["joke"]);
-
-    // .then(text => text.text());
-    // console.log(text('message'));
-    displayQuote(json_response['joke']);
+    let json_response = JSON.parse(response);
+   // console.log(json_response);
+    console.log(json_response['message']);
+    
+   // .then(text => text.text());
+   // console.log(text('message'));
+   displayQuote(json_response['message']);
 }
 
 function displayQuote(x) {
-    const quoteBox = document.querySelector('#js-quote-text');
-    // console.log('display quote working');
-    //const textMessage = document.createTextNode(x);
-
-    //quoteBox.appendChild(textMessage);
-    document.getElementById('js-quote-text').textContent = x;
+    const quoteBox = document.querySelector('#js-quote-text');
+    // console.log('display quote working');
+    //const textMessage = document.createTextNode(x);
+    
+    //quoteBox.appendChild(textMessage);
+    document.getElementById('js-quote-text').textContent = x;
 }
 
 //gets Quote on screen
 getQuote();
-
-
-
