70b1131389b1b42de75cd1f4b04e41e90ec12f3a
kcyeneza <70969620+kcyeneza@users.noreply.github.com>
2022-11-10 15:53:51 -0700

    changes
    changes

--- js/assignment12.js
+++ js/assignment12.js
@@ -1,51 +1,47 @@
-/*const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-
-button.addEventListener('click', updateOutput);
-
-button.addEventListener('click', updateOutput);
-
-function updateOutput() {
-    output.textContent = phone_content.value;
-}*/
-
-
-function check(){
+function check() {
     console.log('test');
-
 }
 
-function submit(){
+function submit() {
     alert(output.textContent);
-
 }
 
-function reset(){
+function reset() {
+    outputInt = 0;
+    output.textContent = outputInt;
+}
+
+function minus() {
+    if (outputInt > 0) {
+    outputInt -=1;
+    output.textContent = outputInt; }
     
-    
-    outputInt=0;
-    const filler_number =output.randomNumber(outputInt);
-    output.textContent(filler_number);
-
 }
 
-
-function randomstuff(){
-
-    outputInt=randomNumber(0,999999999);
-    output.textContent=outputInt;
+function plus() {
+    if (outputInt < 9999999999) {
+    outputInt +=1;
+    output.textContent = outputInt;
+    }
 }
 
-function randomNumber(min,max){
-    const num=  Math.floor(Math.random()* (max-mix +1))+min;
+function random() {
+    outputInt = randomNumber(0, 9999999999);
+    output.textContent = outputInt;
+}
+
+function randomNumber(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
     return num;
-    
-}
+  }
 
-const yesButton= document.querySelector('.yes-button').addEventListener('click', check);
-const noButton= document.querySelector('.no-button').addEventListener('click', reset);
+
 
 const output = document.querySelector('.output');
-let outputInt= parseInt(output.textContent);
+let outputInt = parseInt(output.textContent);
 console.log(outputInt);
+
+
+
+const noButton = document.querySelector('.no-button').addEventListener('click', random);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
