dfa389d55bd0275f86debdf6d3c696448390c506
kcyeneza <70969620+kcyeneza@users.noreply.github.com>
2022-11-10 14:47:21 -0700

    changes
    changes

---
+++ js/assignment12.js
@@ -0,0 +1,49 @@
+/*const button = document.querySelector('.button');
+const output = document.querySelector('.output');
+let phone_content = document.querySelector('.phone');
+
+button.addEventListener('click', updateOutput);
+
+button.addEventListener('click', updateOutput);
+
+function updateOutput() {
+    output.textContent = phone_content.value;
+}*/
+
+
+function check(){
+    console.log('test');
+
+}
+
+function submit(){
+    alert(output.textContent);
+
+}
+
+function reset(){
+    
+    outputInt=0;
+    output.textContent=outputInt;
+
+}
+
+
+function randomstuff(){
+
+    outputInt=randomNumber(0,999999999);
+    output.textContent=outputInt;
+}
+
+function randomNumber(min,max){
+    const num=  Math.floor(Math.random()* (max-mix +1))+min;
+    return num;
+    
+}
+
+const yesButton= document.querySelector('.yes-button').addEventListener('click', check);
+const noButton= document.querySelector('.no-button').addEventListener('click', reset);
+
+const output = document.querySelector('.output');
+let outputInt= parseInt(output.textContent);
+console.log(outputInt);
