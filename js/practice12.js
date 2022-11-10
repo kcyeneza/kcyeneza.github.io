1956d0f825d5d298b92be8c577c7d904fe2de572
kcyeneza <70969620+kcyeneza@users.noreply.github.com>
2022-11-10 16:59:10 -0700

    changes
    changes

--- js/practice12.js
+++ js/practice12.js
@@ -12,10 +12,21 @@ function reset() {
 }
 
 
-function random() {
-    outputInt = randomNumber(0, 999);
+function random1() {
+    outputInt = randomNumber(000, 999);
     output.textContent = outputInt;
 }
+
+function random2() {
+    outputInt = randomNumber(000, 999);
+    output.textContent = outputInt;
+}
+
+function random3() {
+    outputInt = randomNumber(0000, 9999);
+    output.textContent = outputInt;
+}
+
 
 function randomNumber(min, max) {
     const num = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -30,5 +41,7 @@ console.log(outputInt);
 
 
 
-const noButton = document.querySelector('.no1-button').addEventListener('click', random);
+const noButton = document.querySelector('.no1-button').addEventListener('click', random1);
 const submitButton = document.querySelector('.yes1-button').addEventListener('click', submit);
+const no2Button = document.querySelector('.no2-button').addEventListener('click', random2);
+const no3Button = document.querySelector('.no3-button').addEventListener('click', random3);
