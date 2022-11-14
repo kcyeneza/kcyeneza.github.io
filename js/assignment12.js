7710b63c8b13d6b379dac5616e3db79217e31aab
kcyeneza <70969620+kcyeneza@users.noreply.github.com>
2022-11-13 21:56:34 -0700

    changes
    changes

--- js/assignment12.js
+++ js/assignment12.js
@@ -1,15 +1,35 @@
-function check() {
-    console.log('test');
-}
+
 
 function submit1() {
-    alert("Move onto step two.");
+    alert("WE'VE RECORDED THE FOLLOWING: "+output.textContent);
+}
+
+function submit2() {
+    alert("WE'VE RECORDED THE FOLLOWING: "+output2.textContent);
+}
+
+function submit3() {
+    alert("WE'VE RECORDED THE FOLLOWING: "+ output3.textContent);
+}
+function submit4() {
+    alert(" CONGRATS, THE GOVERNMENT HAS YOUR PHONE NUMBER: "+output4.textContent);
 }
 
 function random1() {
-    outputInt = randomNumber(0, 999);
-    output.textContent = outputInt;
+    outputInt1 = randomNumber(100, 999);
+    output.textContent = outputInt1;
 }
+
+function random2() {
+    outputInt2 = randomNumber(100, 999);
+    output2.textContent = outputInt2;
+}
+
+function random3() {
+    outputInt3 = randomNumber(1000, 9999);
+    output3.textContent = outputInt3;
+}
+
 
 function randomNumber(min, max) {
     const num = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -17,12 +37,30 @@ function randomNumber(min, max) {
   }
 
 
+function complete(){
+    outputInt4= output.textContent+"-"+output2.textContent+"-"+output3.textContent;
+    output4.textContent = outputInt4;
+}
 
 const output = document.querySelector('.output');
-let outputInt = parseInt(output.textContent);
-console.log(outputInt);
+let outputInt1 = parseInt(output.textContent);
+
+const output2=document.querySelector('.output2');
+let outputInt2=parseInt(output2.textContent);
+
+const output3=document.querySelector('.output3');
+let outputInt3= parseInt(output3.textContent);
+
+const output4=document.querySelector('.output4');
+let outputInt4= parseInt(output4.textContent);
 
 
 
-const noButton = document.querySelector('.no-button').addEventListener('click', random1);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit1);
+const noButton = document.querySelector('.no1-button').addEventListener('click', random1);
+const no2Button = document.querySelector('.no2-button').addEventListener('click', random2);
+const no3Button = document.querySelector('.no3-button').addEventListener('click', random3);
+const yes4Button=document.querySelector('.yes4-button').addEventListener('click', complete);
+const submitButton1 = document.querySelector('.yes1-button').addEventListener('click', submit1);
+const submitButton2 = document.querySelector('.yes2-button').addEventListener('click', submit2);
+const submitButton3 = document.querySelector('.yes3-button').addEventListener('click', submit3);
+const submitButton4 = document.querySelector('.yes4-button').addEventListener('click', submit4);
