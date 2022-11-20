3d16c3e1008c2d0bf43a37dcadfc9ce84f2ca544
kcyeneza <70969620+kcyeneza@users.noreply.github.com>
2022-11-20 15:01:24 -0700

    changes
    changes

--- js/ica14.js
+++ js/ica14.js
@@ -13,4 +13,7 @@ const aliceTumbling = [
   const alice2 = document.querySelector("#alice2");
   const alice3 = document.querySelector("#alice3");
 
-alice1.animate(aliceTumbling, aliceTiming);
+alice1.animate(aliceTumbling, aliceTiming).finished
+    .then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+    .then(() => alice3.animate(aliceTumbling, aliceTiming).finished)
+    .catch(error => console.error(`Error animating Alices: ${error}`));
