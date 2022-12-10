51137c90e6a6a114463fc7f5e52450918a7c5692
kcyeneza <70969620+kcyeneza@users.noreply.github.com>
2022-12-10 15:23:55 -0700

    changes
    changes

--- midterm.js
+++ midterm.js
@@ -1,38 +1,43 @@
-var i = 0;
-var colorsPalette;
+let x
+let y
+let z
+var canvas
 
+
+// function windowResized() { 
+
+//   resizeCanvas(windowWidth,windowHeight)
+//   if(windowWidth < 450) {
+//     size = 10;
+// } else {
+//     size = 100;
+// }
+  
+// }
 function setup() {
-    colorsPalette = [color(146, 167, 202,30),
-            color(186, 196, 219,30),
-            color(118, 135, 172,30),
-            color(76, 41, 81,30),
-            color(144, 62, 92,30),
-            color(178, 93, 119,30),
-            color(215, 118, 136,30),
-            color(246, 156, 164,30),];
-  var width = 500;
-  var height = 500;
-  var canvas = createCanvas(width, height);
-
-  canvas.parent('p5canvas');
-  // createCanvas(800, 800);
-  background(0,0);
-  ellipseMode(CENTER);
-
+ canvas= createCanvas(windowWidth, windowHeight);
+ canvas.position(300,300);
+background(random(0,255), random(0,255), random(0,255));
+resizeCanvas(windowWidth,windowHeight);
+if(windowWidth < 480) {
+  size = 10;
+} else {
+  size = 100;
+}
+  
 }
 
-function draw() {
-  // rotate(20);
-  fill(colorsPalette[floor(random(8))],100);
-  stroke(colorsPalette[floor(random(8))],255);
-  strokeWeight(0.5);
-  arc(width/2,height/2,linesdata[i].outerRadius*2,linesdata[i].outerRadius*2,
-      linesdata[i].startAngle,linesdata[i].endAngle,PIE)
-  if(i<linesdata.length-1){
-    i++;
-  } else {
-    noLoop();
-  }
+
+function draw(){
+  
+  x=random(0,width)
+  y=random(0,height)
+  z=random(10,60)
+  rect(x,y,z,z)
+ fill(random(0,255), random(0,255), random(0,255),100)
+ stroke(0);
+// stroke(random(150,255), random(150,255), random(150,255))
+  
 
 }
 
